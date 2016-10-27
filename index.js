7fd675e5f310e94fe6a804dc9c7e07116bce2f54
comp500 <comp500@users.noreply.github.com>
2016-10-27 17:50:52 +0100

    test stuff

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.all("/*", requestProxy({
 // resolve actual IP of authserver
 dns.resolve4('authserver.mojang.com', (err, addresses) => {
 	if (err) throw err;
-	console.log(addresses);
 	if (addresses.length > 0) {
 		authserverIP = addresses[0];
 		// Create an HTTPS service
 		https.createServer(options, app).listen(443);
+		console.log("https://" + authserverIP + "/*");
 	} else {
 		throw "No addresses, no upstream";
 	}
