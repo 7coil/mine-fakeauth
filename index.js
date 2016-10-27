bf43a8832f0132fef4f04e3778a9be46e8a81a4e
comp500 <comp500@users.noreply.github.com>
2016-10-27 17:42:54 +0100

    resolve auth server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var https = require('https');
 var fs = require('fs');
 var morgan = require('morgan');
 var requestProxy = require('express-request-proxy');
+var dns = require('dns');
+var authserverIP;
 
 var options = {
 	key: fs.readFileSync('key.pem'),
@@ -15,8 +17,21 @@ var app = express();
 app.use(morgan('combined'));
 
 app.all("/*", requestProxy({
-	url: "https://authserver.mojang.com/*"
+	url: "https://" + authserverIP + "/*",
+	headers: {
+		"Host": "authserver.mojang.com"
+	}
 }));
+
+// resolve actual IP of authserver
+dns.resolve4('authserver.mojang.com', (err, addresses) => {
+  if (err) throw err;
+  if (addresses.length > 0) {
+	  authserverIP = addresses[0];
+  } else {
+	  throw "No addresses, no upstream";
+  }
+});
 
 // Create an HTTPS service
 https.createServer(options, app).listen(443);
