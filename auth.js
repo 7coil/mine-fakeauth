9080b040a7f70cfd4ce4d52852e92fb63db6373a
comp500 <comp500@users.noreply.github.com>
2016-10-28 08:28:48 +0100

    separate auth code from rest of document

---
+++ auth.js
@@ -0,0 +1,29 @@
+module.exports = {};
+
+module.exports.authenticate(data) {
+
+}
+
+module.exports.checkRefresh(data) {
+
+}
+
+module.exports.refresh(data) {
+
+}
+
+module.exports.checkValidate(data) {
+
+}
+
+module.exports.validate(data) {
+
+}
+
+module.exports.checkInvalidate(data) {
+
+}
+
+module.exports.invalidate(data) {
+
+}
