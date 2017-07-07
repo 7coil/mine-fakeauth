fd9d0f3564457f8b567994ba1156ca231e283c3e
comp500 <comp500@users.noreply.github.com>
2017-07-07 19:44:15 +0100

    create dummy db file

---
+++ databases/dummy.js
@@ -0,0 +1,25 @@
+module.exports = {};
+
+module.exports.getPlayerFromLogin = function (username, password) {
+
+};
+
+module.exports.getPlayerFromToken = function (accessToken) {
+
+};
+
+module.exports.getAccessToken = function (clientToken) {
+
+};
+
+module.exports.setAccessToken = function (clientToken) {
+
+};
+
+module.exports.deleteTokensFromUser = function (username) {
+
+};
+
+module.exports.deleteTokenFromToken = function (accessToken) {
+
+};
