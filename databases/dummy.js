5289f43dc40d8e8694a20bc3d1d8fe5154f3fa5b
comp500 <comp500@users.noreply.github.com>
2017-07-07 20:20:24 +0100

    add promise skeletons, database objects

--- databases/dummy.js
+++ databases/dummy.js
@@ -1,11 +1,24 @@
+var playerDatabase = [
+	{
+		username: "comp500",
+		password: "password",
+		profileID: "433d80f8-3ea7-4e67-9fac-0a39821aa870"
+	}
+];
+var tokenDatabase = [];
+
 module.exports = {};
 
 module.exports.getPlayerFromLogin = function (username, password) {
+	return new Promise(function (resolve, reject) {
 
+	});
 };
 
 module.exports.getPlayerFromToken = function (accessToken) {
+	return new Promise(function (resolve, reject) {
 
+	});
 };
 
 module.exports.getAccessToken = function (clientToken) {
@@ -13,13 +26,19 @@ module.exports.getAccessToken = function (clientToken) {
 };
 
 module.exports.setAccessToken = function (clientToken) {
+	return new Promise(function (resolve, reject) {
 
+	});
 };
 
 module.exports.deleteTokensFromUser = function (username) {
+	return new Promise(function (resolve, reject) {
 
+	});
 };
 
 module.exports.deleteTokenFromToken = function (accessToken) {
+	return new Promise(function (resolve, reject) {
 
+	});
 };
