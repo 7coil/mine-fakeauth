37d32681e82527e0b07b9b11d076227a91159141
comp500 <comp500@users.noreply.github.com>
2017-07-07 21:39:53 +0100

    keys directory, test pem

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 var data = require("./databases/dummy.js");
 var web = require("./libs/web.js");
 var endpoints = require("./libs/endpoints.js");
-var webrun = web();
+var fs = require("fs");
+
+var webrun = web({
+	https: {
+		key: fs.readFileSync('./keys/key.pem'),
+		cert: fs.readFileSync('./keys/cert.pem')
+	}
+});
 
 endpoints(webrun, data);
