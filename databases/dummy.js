0dd1c59e610c063c91ca032c39c213f4b185e486
comp500 <comp500@users.noreply.github.com>
2017-07-07 20:41:54 +0100

    implement dummy database

--- databases/dummy.js
+++ databases/dummy.js
@@ -30,28 +30,73 @@ module.exports.getPlayerFromLogin = function (username, password) {
 
 module.exports.getPlayerFromToken = function (accessToken) {
 	return new Promise(function (resolve, reject) {
-
+		var token = tokenDatabase.find(function (element) {
+			return accessToken == element.accessToken;
+		});
+		if (token) {
+			var player = playerDatabase.find(function (element) {
+				return token.username == element.username;
+			});
+			if (player) {
+				resolve(player);
+			} else {
+				reject(); // this shouldn't happen
+			}
+		} else {
+			reject();
+		}
 	});
 };
 
 module.exports.getAccessToken = function (clientToken) {
-
+	var token = tokenDatabase.find(function (element) {
+		return clientToken == element.clientToken;
+	});
+	if (token) {
+		return token.accessToken;
+	} else {
+		return null;
+	}
 };
 
-module.exports.setAccessToken = function (clientToken) {
+module.exports.setAccessToken = function (clientToken, accessToken, username) {
 	return new Promise(function (resolve, reject) {
-
+		var token = tokenDatabase.findIndex(function (element) {
+			return clientToken == element.clientToken;
+		});
+		if (token > -1) {
+			tokenDatabase.splice(token, 1);
+		}
+		tokenDatabase.push({
+			clientToken: clientToken,
+			accessToken: accessToken,
+			username: username
+		});
 	});
 };
 
 module.exports.deleteTokensFromUser = function (username) {
 	return new Promise(function (resolve, reject) {
-
+		do {
+			var token = tokenDatabase.findIndex(function (element) {
+				return username == element.username;
+			});
+			if (token > -1) {
+				tokenDatabase.splice(token, 1);
+			}
+		} while (token > -1);
+		resolve();
 	});
 };
 
 module.exports.deleteTokenFromToken = function (accessToken) {
 	return new Promise(function (resolve, reject) {
-
+		var token = tokenDatabase.findIndex(function (element) {
+			return accessToken == element.accessToken;
+		});
+		if (token > -1) {
+			tokenDatabase.splice(token, 1);
+		}
+		resolve();
 	});
 };
