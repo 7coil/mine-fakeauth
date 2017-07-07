f02a334fe71a75484a116b970a98beb6186795f9
comp500 <comp500@users.noreply.github.com>
2017-07-07 15:59:14 +0100

    json, use http for test

--- libs/web.js
+++ libs/web.js
@@ -16,25 +16,25 @@ module.exports = function () {
 	app.use(function(req, res, next) {
 		if (req.method != "POST") { // differing from reference implementation as 404 returned before 405 normally
 			res.status(405);
-			res.end({
+			res.end(JSON.stringify({
 				"error": "Method Not Allowed",
 				"errorMessage": "The method specified in the request is not allowed for the resource identified by the request URI",
-			});
+			}));
 		} else {
 			res.status(404);
-			res.end({
+			res.end(JSON.stringify({
 				"error": "Not Found",
 				"errorMessage": "The server has not found anything matching the request URI",
-			});
+			}));
 		}
 	});
 	
-	https.createServer(options, app).listen(443);
+	http.createServer(app).listen(443);
 	
 	return function (endpoint, callback) {
 		router.post(endpoint, function (req, res) {
 			callback(req.body, function endpointHandler(status, response) {
-				res.end(response);
+				res.end(JSON.stringify(response));
 			}, function errorHandler(error) {
 				red.status(500); // TODO send/handle errors
 			});
