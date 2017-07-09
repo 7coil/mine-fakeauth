9730be068c2fee4be75f4f19849b5dcd7ca4c10b
comp500 <comp500@users.noreply.github.com>
2017-07-09 16:32:50 +0100

    don't need authlib

--- test.js
+++ test.js
@@ -2,18 +2,12 @@ var data = require("./databases/dummy.js");
 var web = require("./libs/web.js");
 var endpoints = require("./libs/endpoints.js");
 var fs = require("fs");
-var router = require("express").Router();
-
-router.get("/com/mojang/authlib/:ver/authlib-:ver.jar", function () {
-	res.sendFile("./keys/authlib-1.5.22.jar");
-});
 
 var webrun = web({
 	https: {
 		key: fs.readFileSync('./keys/key.key'),
 		cert: fs.readFileSync('./keys/cert.crt')
-	},
-	extraRouter: router
+	}
 });
 
 endpoints(webrun, data);
