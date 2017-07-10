06f6bbbc0536a58859d62bd65157d05e6addc785
comp500 <comp500@users.noreply.github.com>
2017-07-10 18:30:41 +0100

    go back to done functions, mocha is stupid

--- test/endpointsTest.js
+++ test/endpointsTest.js
@@ -5,59 +5,47 @@ const noop = function () {
 };
 var endpoints = {};
 
-// let mocha handle rejections
-process.on('unhandledRejection', function (reason) {
-    throw reason;
-});
-
 // import all endpoint functions to endpoints
 endpointsFile(function (url, callback) {
 	endpoints[url] = callback;
 }, require("../databases/dummy.js"));
 
 describe("authenticate", function () {
-	it("should fail on invalid login", function () {
-		return new Promise(function (resolve) {
-			endpoints["/authenticate"]({
-				"username": "invalid",
-				"password": "invalid"
-			}, noop, function (error) {
-				assert.equal(error, 3);
-				resolve();
-			});
+	it("should fail on invalid login", function (done) {
+		endpoints["/authenticate"]({
+			"username": "invalid",
+			"password": "invalid"
+		}, noop, function (error) {
+			assert.equal(error, 3);
+			done();
 		});
 	});
 
-	it("should succeed on valid login", function () {
-		return new Promise(function (resolve) {
-			endpoints["/authenticate"]({
-				"username": "comp500",
-				"password": "password"
-			}, function (result) {
-				assert.isObject(result);
-				assert.isString(result.accessToken);
-				assert.isString(result.clientToken);
-				resolve();
-			}, noop);
-		});
+	it("should succeed on valid login", function (done) {
+		endpoints["/authenticate"]({
+			"username": "comp500",
+			"password": "password"
+		}, function (result) {
+			assert.isObject(result);
+			assert.isString(result.accessToken);
+			assert.isString(result.clientToken);
+			done();
+		}, noop);
 	});
 
-	it("should provide profiles if agent provided", function () {
-		return new Promise(function (resolve) {
-			endpoints["/authenticate"]({
-				"username": "comp500",
-				"password": "password",
-				"agent": {
-					"name": "Minecraft",
-					"version": 1
-				}
-			}, function (result) {
-				throw new Error();
-				assert.containsAllKeys(result.selectedProfile, ["id", "name"]);
-				assert.containsAllKeys(result.availableProfiles[0], ["id", "name"]);
-				//assert.equal(result.selectedProfile, result.availableProfiles[0]);
-				resolve();
-			}, noop);
-		});
+	it("should provide profiles if agent provided", function (done) {
+		endpoints["/authenticate"]({
+			"username": "comp500",
+			"password": "password",
+			"agent": {
+				"name": "Minecraft",
+				"version": 1
+			}
+		}, function (result) {
+			assert.containsAllKeys(result.selectedProfile, ["id", "name"]);
+			assert.containsAllKeys(result.availableProfiles[0], ["id", "name"]);
+			assert.equal(result.selectedProfile, result.availableProfiles[0]);
+			done();
+		}, noop);
 	});
 });
