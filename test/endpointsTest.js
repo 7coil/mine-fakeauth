aa670f4e9b20e5771fe9ad510494aa36073feaf5
comp500 <comp500@users.noreply.github.com>
2017-07-10 18:09:21 +0100

    add agent test, use promises?

--- test/endpointsTest.js
+++ test/endpointsTest.js
@@ -5,6 +5,11 @@ const noop = function () {
 };
 var endpoints = {};
 
+// let mocha handle rejections
+process.on('unhandledRejection', function (reason) {
+    throw reason;
+});
+
 // import all endpoint functions to endpoints
 endpointsFile(function (url, callback) {
 	endpoints[url] = callback;
@@ -12,22 +17,47 @@ endpointsFile(function (url, callback) {
 
 describe("authenticate", function () {
 	it("should fail on invalid login", function () {
-		endpoints["/authenticate"]({
-			"username": "invalid",
-			"password": "invalid"
-		}, noop, function (error) {
-			assert.equal(error, 3);
+		return new Promise(function (resolve) {
+			endpoints["/authenticate"]({
+				"username": "invalid",
+				"password": "invalid"
+			}, noop, function (error) {
+				assert.equal(error, 3);
+				resolve();
+			});
 		});
 	});
 
 	it("should succeed on valid login", function () {
-		endpoints["/authenticate"]({
-			"username": "comp500",
-			"password": "password"
-		}, function (result) {
-			assert.isObject(result);
-			assert.isString(result.accessToken);
-			assert.isString(result.clientToken);
-		}, noop);
+		return new Promise(function (resolve) {
+			endpoints["/authenticate"]({
+				"username": "comp500",
+				"password": "password"
+			}, function (result) {
+				assert.isObject(result);
+				assert.isString(result.accessToken);
+				assert.isString(result.clientToken);
+				resolve();
+			}, noop);
+		});
+	});
+
+	it("should provide profiles if agent provided", function () {
+		return new Promise(function (resolve) {
+			endpoints["/authenticate"]({
+				"username": "comp500",
+				"password": "password",
+				"agent": {
+					"name": "Minecraft",
+					"version": 1
+				}
+			}, function (result) {
+				throw new Error();
+				assert.containsAllKeys(result.selectedProfile, ["id", "name"]);
+				assert.containsAllKeys(result.availableProfiles[0], ["id", "name"]);
+				//assert.equal(result.selectedProfile, result.availableProfiles[0]);
+				resolve();
+			}, noop);
+		});
 	});
 });
