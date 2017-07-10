d9b086f9f77dcaa7c247e237274b51945ae56d43
comp500 <comp500@users.noreply.github.com>
2017-07-10 18:55:07 +0100

    copy paste for refresh

--- test/endpointsTest.js
+++ test/endpointsTest.js
@@ -120,3 +120,114 @@ describe("authenticate", function () {
 		}, noop);
 	});
 });
+
+describe("refresh", function () {
+	it("should fail on invalid tokens", function (done) {
+		endpoints["/refresh"]({
+			"accessToken": "invalid",
+			"clientToken": "invalid"
+		}, noop, function (error) {
+			assert.equal(error, 5);
+			done();
+		});
+	});
+
+	it("should fail on no tokens", function (done) {
+		endpoints["/refresh"]({}, noop, function (error) {
+			assert.equal(error, 5);
+			done();
+		});
+	});
+
+	it("should fail on null tokens", function (done) {
+		endpoints["/refresh"]({
+			"accessToken": null,
+			"clientToken": null
+		}, noop, function (error) {
+			assert.equal(error, 5);
+			done();
+		});
+	});
+
+	/*it("should succeed on valid login", function (done) {
+		endpoints["/refresh"]({
+			"username": "comp500",
+			"password": "password"
+		}, function (result) {
+			assert.isObject(result);
+			assert.isString(result.accessToken);
+			assert.isString(result.clientToken);
+			done();
+		}, noop);
+	});
+
+	it("should keep equal clientToken", function (done) {
+		endpoints["/refresh"]({
+			"username": "comp500",
+			"password": "password",
+			"clientToken": "totallyAToken"
+		}, function (result) {
+			assert.isString(result.accessToken);
+			assert.isString(result.clientToken);
+			assert.equal(result.clientToken, "totallyAToken");
+			done();
+		}, noop);
+	});
+
+	it("should keep same accessToken after multiple requests", function (done) {
+		endpoints["/refresh"]({
+			"username": "comp500",
+			"password": "password",
+			"clientToken": "totallyAToken"
+		}, function (result) {
+			endpoints["/refresh"]({
+				"username": "comp500",
+				"password": "password",
+				"clientToken": "totallyAToken"
+			}, function (result2) {
+				assert.equal(result.clientToken, "totallyAToken");
+				assert.equal(result2.clientToken, "totallyAToken");
+				assert.equal(result.accessToken, result2.accessToken);
+				done();
+			}, noop);
+		}, noop);
+	});
+
+	it("should provide profiles if agent provided", function (done) {
+		endpoints["/refresh"]({
+			"username": "comp500",
+			"password": "password",
+			"agent": {
+				"name": "Minecraft",
+				"version": 1
+			}
+		}, function (result) {
+			assert.isString(result.accessToken);
+			assert.isString(result.clientToken);
+			assert.isString(result.selectedProfile.id);
+			assert.isString(result.selectedProfile.name);
+			assert.deepEqual(result.selectedProfile, result.availableProfiles[0]);
+			assert.equal(result.selectedProfile.name, "comp500");
+			done();
+		}, noop);
+	});
+
+	it("should provide user if requested", function (done) {
+		endpoints["/refresh"]({
+			"username": "comp500",
+			"password": "password",
+			"requestUser": true,
+			"agent": {
+				"name": "Minecraft",
+				"version": 1
+			}
+		}, function (result) {
+			assert.isString(result.accessToken);
+			assert.isString(result.clientToken);
+			assert.isString(result.selectedProfile.id);
+			assert.equal(result.user.id, result.selectedProfile.id);
+			assert.equal(result.user.id, result.availableProfiles[0].id);
+			done();
+		}, noop);
+	});*/
+});
